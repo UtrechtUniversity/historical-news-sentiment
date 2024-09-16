1e586c417bd8187e078ebfdeb71559243f340886
Shiva Nadi <s.nadi@uu.nl>
2024-09-16 15:55:15 +0200

    Add plot_word_vectors

--- interest/sentiment.py
+++ interest/sentiment.py
@@ -11,6 +11,8 @@ import multiprocessing
 from time import time
 from tqdm import tqdm
 import string
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 
 # logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s", datefmt= '%H:%M:%S', level=logging.INFO)
@@ -43,52 +45,170 @@ class SentimentAnalyser:
             negative_words = f.read().splitlines()
         return positive_words, negative_words
 
-    # def _preprocess_text(self, text) -> str:
-    #     """Preprocess text by decapitalizing and removing specific punctuation."""
-    #     text = text.lower()
-        
-    #     # punctuation_to_remove = string.punctuation.replace('.', '')
-    #     # preprocessed_text = text.translate(str.maketrans('', '', punctuation_to_remove))
-        
-    #     return text
-
-
-    def text_to_word_vectors(self) -> List[np.ndarray]:
-        word_vectors = []
+    def text_to_word_vectors(self) -> List[List[np.ndarray]]:
+        articles_word_vectors = []
         missing_words = set()
 
-        # Apply preprocessing to each sentence
-        sentences = [word.lower() for article in self.articles 
-                                                       for sentence in article 
-                                                       for word in sentence.split()]
-        
-        for word in sentences:
-            if word in self.w2v_pretrained_model.key_to_index:
-                word_vectors.append(self.w2v_pretrained_model[word])
-            else:
-                missing_words.add(word)
+        for article in tqdm(self.articles, desc="Calculating word vectors...", unit="article"):
+            article_word_vectors = []
+            for sentence in article:
+                sentence_word_vectors = []
+                for word in sentence.split():
+                    word = word.lower()
+                    if word in self.w2v_pretrained_model.key_to_index:
+                        sentence_word_vectors.append(self.w2v_pretrained_model[word])
+                    else:
+                        missing_words.add(word)
+                
+                # Calculate the average vector for the sentence
+                if sentence_word_vectors:
+                    avg_vector = np.mean(sentence_word_vectors, axis=0)
+                    article_word_vectors.append(avg_vector)
+                    logging.info(f"Processed sentence with {len(sentence_word_vectors)} word vectors.")
+                else:
+                    avg_vector = np.zeros(self.w2v_pretrained_model.vector_size)
+                    article_word_vectors.append(avg_vector)
+                    logging.warning("No valid word vectors found for a sentence; appended zero vector.")
+                    
+            articles_word_vectors.append(article_word_vectors)
 
         if missing_words:
             logging.warning(f"{len(missing_words)} words not found in the model: {', '.join(missing_words)}")
         
-        return word_vectors
+        return articles_word_vectors
 
-    
-    # def train_word2vec(self, vector_size: int = 100, window: int = 5, min_count: int = 1):
-    #     """Train a Word2Vec model on the sentences of the news articles."""
-    #     # Flatten the list of lists into a single list of sentences
-    #     sentences = [sentence.split() for article in self.articles for sentence in article]
-    #     self.model = Word2Vec(sentences=sentences, vector_size=vector_size, window=window, min_count=min_count)
+    def positive_words_to_word_vectors(self) -> np.ndarray:
+        positive_word_vectors = []
+        missing_positive_words = set()
 
-    # def _average_vector(self, words: List[str]) -> np.ndarray:
-    #     """Compute the average vector for a list of words."""
-    #     vectors = [self.model.wv[word] for word in words if word in self.model.wv]
-    #     if vectors:
-    #         return np.mean(vectors, axis=0)
-    #     else:
-    #         return np.zeros(self.model.vector_size)
+        for word in self.positive_words:
+            word = word.lower()
+            if word in self.w2v_pretrained_model.key_to_index:
+                positive_word_vectors.append(self.w2v_pretrained_model[word])
+            else:
+                missing_positive_words.add(word)
 
+        if positive_word_vectors:
+                    positive_word_vector = np.mean(positive_word_vectors, axis=0)
+                    logging.info(f"Processed sentence with {len(positive_word_vectors)} word vectors.")
+        else:
+            positive_word_vector = np.zeros(self.w2v_pretrained_model.vector_size)
+            logging.warning("No valid word vectors found for a sentence; appended zero vector.")
+            
+
+        if missing_positive_words:
+            logging.warning(f"{len(missing_positive_words)} words not found in the model: {', '.join(missing_positive_words)}")
+        
+        return positive_word_vector
+
+    def negative_words_to_word_vectors(self) -> np.ndarray:
+        negative_word_vectors = []
+        missing_negative_words = set()
+
+        for word in self.negative_words:
+            word = word.lower()
+            if word in self.w2v_pretrained_model.key_to_index:
+                negative_word_vectors.append(self.w2v_pretrained_model[word])
+            else:
+                missing_negative_words.add(word)
+
+        if negative_word_vectors:
+                    negative_word_vector = np.mean(negative_word_vectors, axis=0)
+                    logging.info(f"Processed sentence with {len(negative_word_vectors)} word vectors.")
+        else:
+            negative_word_vector = np.zeros(self.w2v_pretrained_model.vector_size)
+            logging.warning("No valid word vectors found for a sentence; appended zero vector.")
+            
+
+        if missing_negative_words:
+            logging.warning(f"{len(missing_negative_words)} words not found in the model: {', '.join(missing_negative_words)}")
+        
+        return negative_word_vector
+
+
+    def plot_word_vectors(self, positive_word_vector: np.ndarray, negative_word_vector: np.ndarray):
+        """Reduce dimensions using PCA and plot the positive and negative word vectors."""
+        
+        # Combine the positive and negative word vectors for PCA
+        vectors = np.vstack([positive_word_vector, negative_word_vector])
+
+        # Use PCA to reduce dimensionality to 2 components for visualization
+        pca = PCA(n_components=2)
+        reduced_vectors = pca.fit_transform(vectors)
+
+        # Create a scatter plot
+        plt.figure(figsize=(8, 6))
+        plt.scatter(reduced_vectors[0, 0], reduced_vectors[0, 1], color='green', label='Positive Sentiment')
+        plt.scatter(reduced_vectors[1, 0], reduced_vectors[1, 1], color='red', label='Negative Sentiment')
+
+        # Label the plot
+        plt.title('2D PCA of Positive and Negative Word Vectors')
+        plt.xlabel('Principal Component 1')
+        plt.ylabel('Principal Component 2')
+        plt.legend()
+        plt.show()
+
+    def calculate_similarity(self, articles_word_vectors, negative_sentiment_word_vector, positive_sentiment_word_vector):
+        # Check dimensionality
+        vector_dim = positive_sentiment_word_vector.shape[0]
+        if negative_sentiment_word_vector.shape[0] != vector_dim:
+            raise ValueError("Dimensionality mismatch: positive and negative sentiment vectors must have the same dimensions.")
+        
+        # Initialize lists to store positive and negative similarities
+        pos_similarities = []
+        neg_similarities = []
+        article_sentiment = []
+
+        for article in articles_word_vectors:
+            for sentence_vector in article:
+                # Convert sentence_vector to a NumPy array
+                sentence_vector = np.array(sentence_vector)
+
+                # Check dimensionality
+                if len(sentence_vector) != vector_dim:
+                    print(len(sentence_vector))
+                    print(vector_dim)
+                    raise ValueError("Dimensionality mismatch: sentence vectors must have the same dimensions as the sentiment vectors.")
+                
+                # Reshape vectors to be 2D (necessary for cosine_similarity)
+                sentence_vector_reshaped = sentence_vector.reshape(1, -1)
+                positive_vector_reshaped = positive_sentiment_word_vector.reshape(1, -1)
+                negative_vector_reshaped = negative_sentiment_word_vector.reshape(1, -1)
+                
+                # Calculate cosine similarity for each sentence with both positive and negative sentiment vectors
+                pos_similarity = cosine_similarity(sentence_vector_reshaped, positive_vector_reshaped)[0][0]
+                neg_similarity = cosine_similarity(sentence_vector_reshaped, negative_vector_reshaped)[0][0]
+
+                # Append similarities to the respective lists
+                pos_similarities.append(pos_similarity)
+                neg_similarities.append(neg_similarity)
+
+            # Compute the mean similarity for the entire article
+            mean_pos_similarity = np.mean(pos_similarities)
+            mean_neg_similarity = np.mean(neg_similarities)
+            article_sentiment.append(mean_pos_similarity - mean_neg_similarity)
+        print(article_sentiment)
+
+        # # Compare the mean similarities and print the overall sentiment for the article
+        # if mean_pos_similarity > mean_neg_similarity:
+        #     print(f"Article is more positive (mean_pos_similarity: {mean_pos_similarity}, mean_neg_similarity: {mean_neg_similarity}).")
+        # elif mean_pos_similarity < mean_neg_similarity:
+        #     print(f"Article is more negative (mean_pos_similarity: {mean_pos_similarity}, mean_neg_similarity: {mean_neg_similarity}).")
+        # else:
+        #     print(f"Article is neutral (mean_pos_similarity: {mean_pos_similarity}, mean_neg_similarity: {mean_neg_similarity}).")
 
 if __name__ == '__main__':
     analyzer = SentimentAnalyser('../data/negative_words_gpt.txt', '../data/positive_words_gpt.txt', '../data/merged/coal/1960s_coal.csv')
-    print(analyzer.text_to_word_vectors()[0])
+    aritcles_word_vectors = analyzer.text_to_word_vectors()
+    num_articles = len(aritcles_word_vectors)
+    num_sentence_vectors_per_article = len(aritcles_word_vectors[0])
+    sentence_vector_shape = len(aritcles_word_vectors[0][0])
+
+    print(f"Number of articles: {num_articles}")
+    print(f"Number of sentence vectors per article: {num_sentence_vectors_per_article}")
+    print(f"Shape of sentence vectors: {sentence_vector_shape}")
+
+    negative_sentiment_word_vector = analyzer.negative_words_to_word_vectors()
+    positive_sentiment_word_vector = analyzer.positive_words_to_word_vectors()
+    analyzer.calculate_similarity(aritcles_word_vectors, negative_sentiment_word_vector, positive_sentiment_word_vector)
+    # analyzer.plot_word_vectors(pos, neg)
