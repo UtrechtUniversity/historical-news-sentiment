bf594a1632f5ca045b502c90a85ebea7204ade6a
Shiva Nadi <s.nadi@uu.nl>
2024-09-26 13:57:20 +0200

    Initial commit

---
+++ calculate_sentiments.py
@@ -0,0 +1,29 @@
+from interest.sentiment import SentimentAnalyser
+from argparse import ArgumentParser
+from pathlib import Path
+
+
+def parse_arguments():
+    parser = ArgumentParser(
+        prog="csentiment_analysis.py",
+        description="Calculate sentiments")
+    parser.add_argument("--negative_words_dir", required=True)
+    parser.add_argument("--positive_words_dir", required=True)
+    parser.add_argument("--articles_dir", required=True)
+    return parser.parse_args()
+
+
+def compute_sentiment(negative_words_dir, positive_words_dir, articles_dir):
+    analyzer = SentimentAnalyser('data/negative_words_gpt.txt', 'data/positive_words_gpt.txt', 'data/merged/combined_df.csv')
+    
+    negative_sentiment_word_vector = analyzer.negative_words_to_word_vectors()
+    positive_sentiment_word_vector = analyzer.positive_words_to_word_vectors()
+    articles_word_vectors = analyzer.text_to_word_vectors()
+    analyzer.plot_word_vectors(negative_sentiment_word_vector, positive_sentiment_word_vector, articles_word_vectors)
+    article_sentiments = analyzer.calculate_article_sentiment(articles_word_vectors, negative_sentiment_word_vector, positive_sentiment_word_vector, neutral_threshold=0.05)
+    analyzer.evaluate_sentiment_predictions(analyzer.sentiment_labels, article_sentiments)
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    compute_sentiment(Path(args.negative_words_dir), Path(args.positive_words_dir),
+                       Path(args.articles_dir))
