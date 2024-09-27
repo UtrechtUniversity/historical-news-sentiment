9ab917749bf29f61faad4af07a091370dd9d1a22
Shiva Nadi <s.nadi@uu.nl>
2024-09-27 10:00:15 +0200

    Fix flake8 issues

--- interest/sentiment.py
+++ interest/sentiment.py
@@ -1,42 +1,42 @@
-from typing import List, Tuple
-import  pandas as pd
+from typing import List
+import pandas as pd
 import numpy as np
 import gensim
-from gensim.models import Word2Vec
-from sklearn.metrics.pairwise import cosine_similarity
 import nltk
 from nltk.tokenize import sent_tokenize
 import logging
-import multiprocessing
-from time import time
 from tqdm import tqdm
-import string
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-from nltk.tokenize import sent_tokenize
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
-import seaborn as sns
-
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    classification_report,
+    confusion_matrix,
+    ConfusionMatrixDisplay
+)
 
 nltk.download('punkt')
 
+
 class SentimentAnalyser:
-    def __init__(self, negative_words_fp: List[str], positive_words_fp: List[str], articles_fp: str, model_fp: str):
+    def __init__(self, negative_words_fp: List[str], positive_words_fp: List[str], articles_fp: str, model_fp: str):  # noqa: E501
         self.negative_words_fp = negative_words_fp
         self.positive_words_fp = positive_words_fp
         self.positive_words, self.negative_words = self._load_sentiment_words()
         self.articles_fp = articles_fp
         self.articles, self.sentiment_labels = self._load_articles()
-        self.w2v_pretrained_model = gensim.models.KeyedVectors.load_word2vec_format(
+        self.w2v_pretrained_model = gensim.models.KeyedVectors.load_word2vec_format(  # noqa: E501
             model_fp, binary=True)
         logging.basicConfig(
             level=logging.INFO,
             format='%(asctime)s - %(levelname)s - %(message)s',
             datefmt='%H:%M:%S',
-            filename='analyser.log', 
-            filemode='w'  # 'a' to append, 'w' to overwrite the log file each time
+            filename='analyser.log',
+            filemode='w'  # 'a' to append, 'w' to overwrite the log file each time  # noqa: E501
         )
-
 
     def _load_articles(self) -> List[List[str]]:
         """Load articles from the CSV file and return a list of sentences."""
@@ -57,32 +57,32 @@ class SentimentAnalyser:
         articles_word_vectors = []
         missing_words = set()
 
-        for article in tqdm(self.articles, desc="Calculating word vectors...", unit="article"):
+        for article in tqdm(self.articles, desc="Calculating word vectors...", unit="article"):  # noqa: E501
             article_word_vectors = []
             for sentence in article:
                 sentence_word_vectors = []
                 for word in sentence.split():
                     word = word.lower()
                     if word in self.w2v_pretrained_model.key_to_index:
-                        sentence_word_vectors.append(self.w2v_pretrained_model[word])
+                        sentence_word_vectors.append(self.w2v_pretrained_model[word])  # noqa: E501
                     else:
                         missing_words.add(word)
-                
+
                 # Calculate the average vector for the sentence
                 if sentence_word_vectors:
                     avg_vector = np.mean(sentence_word_vectors, axis=0)
                     article_word_vectors.append(avg_vector)
-                    logging.info(f"Processed sentence with {len(sentence_word_vectors)} word vectors.")
+                    logging.info(f"Processed sentence with {len(sentence_word_vectors)} word vectors.")  # noqa: E501
                 else:
-                    avg_vector = np.zeros(self.w2v_pretrained_model.vector_size)
+                    avg_vector = np.zeros(self.w2v_pretrained_model.vector_size)  # noqa: E501
                     article_word_vectors.append(avg_vector)
-                    logging.warning("No valid word vectors found for a sentence; appended zero vector.")
-                    
+                    logging.warning("No valid word vectors found for a sentence; appended zero vector.")  # noqa: E501
+
             articles_word_vectors.append(article_word_vectors)
 
         if missing_words:
-            logging.warning(f"{len(missing_words)} words not found in the model: {', '.join(missing_words)}")
-        
+            logging.warning(f"{len(missing_words)} words not found in the model: {', '.join(missing_words)}")  # noqa: E501
+
         return articles_word_vectors
 
     def positive_words_to_word_vectors(self) -> np.ndarray:
@@ -97,16 +97,15 @@ class SentimentAnalyser:
                 missing_positive_words.add(word)
 
         if positive_word_vectors:
-                    positive_word_vector = np.mean(positive_word_vectors, axis=0)
-                    logging.info(f"Processed sentence with {len(positive_word_vectors)} word vectors.")
+            positive_word_vector = np.mean(positive_word_vectors, axis=0)
+            logging.info(f"Processed sentence with {len(positive_word_vectors)} word vectors.")  # noqa: E501
         else:
-            positive_word_vector = np.zeros(self.w2v_pretrained_model.vector_size)
-            logging.warning("No valid word vectors found for a sentence; appended zero vector.")
-            
+            positive_word_vector = np.zeros(self.w2v_pretrained_model.vector_size)  # noqa: E501
+            logging.warning("No valid word vectors found for a sentence; appended zero vector.")  # noqa: E501
 
         if missing_positive_words:
-            logging.warning(f"{len(missing_positive_words)} words not found in the model: {', '.join(missing_positive_words)}")
-        
+            logging.warning(f"{len(missing_positive_words)} words not found in the model: {', '.join(missing_positive_words)}")  # noqa: E501
+
         return positive_word_vector
 
     def negative_words_to_word_vectors(self) -> np.ndarray:
@@ -121,24 +120,23 @@ class SentimentAnalyser:
                 missing_negative_words.add(word)
 
         if negative_word_vectors:
-                    negative_word_vector = np.mean(negative_word_vectors, axis=0)
-                    logging.info(f"Processed sentence with {len(negative_word_vectors)} word vectors.")
+            negative_word_vector = np.mean(negative_word_vectors, axis=0)  # noqa: E501
+            logging.info(f"Processed sentence with {len(negative_word_vectors)} word vectors.")  # noqa: E501
         else:
-            negative_word_vector = np.zeros(self.w2v_pretrained_model.vector_size)
-            logging.warning("No valid word vectors found for a sentence; appended zero vector.")
-            
+            negative_word_vector = np.zeros(self.w2v_pretrained_model.vector_size)  # noqa: E501
+            logging.warning("No valid word vectors found for a sentence; appended zero vector.")  # noqa: E501
 
         if missing_negative_words:
-            logging.warning(f"{len(missing_negative_words)} words not found in the model: {', '.join(missing_negative_words)}")
-        
+            logging.warning(f"{len(missing_negative_words)} words not found in the model: {', '.join(missing_negative_words)}")  # noqa: E501
+
         return negative_word_vector
 
-    def plot_word_vectors(self, positive_word_vector: np.ndarray, negative_word_vector: np.ndarray, articles_word_vectors: List[List[np.ndarray]]):
-        """Reduce dimensions using PCA and plot the positive, negative, and text word vectors."""
-        
-        flattened_articles_word_vectors = [vec for sublist in articles_word_vectors for vec in sublist]
-        articles_word_vectors_array = np.vstack(flattened_articles_word_vectors)
-        vectors = np.vstack([positive_word_vector, negative_word_vector, articles_word_vectors_array])
+    def plot_word_vectors(self, positive_word_vector: np.ndarray, negative_word_vector: np.ndarray, articles_word_vectors: List[List[np.ndarray]]):  # noqa: E501
+        """Reduce dimensions using PCA and plot the positive, negative, and text word vectors."""  # noqa: E501
+
+        flattened_articles_word_vectors = [vec for sublist in articles_word_vectors for vec in sublist]  # noqa: E501
+        articles_word_vectors_array = np.vstack(flattened_articles_word_vectors)  # noqa: E501
+        vectors = np.vstack([positive_word_vector, negative_word_vector, articles_word_vectors_array])  # noqa: E501
 
         # Use PCA to reduce dimensionality to 2 components for visualization
         pca = PCA(n_components=2)
@@ -147,11 +145,11 @@ class SentimentAnalyser:
         plt.figure(figsize=(8, 6))
 
         # Plot text word vectors
-        plt.scatter(reduced_vectors[2:, 0], reduced_vectors[2:, 1], color='blue', label='Text Word Vectors', alpha=0.6)
+        plt.scatter(reduced_vectors[2:, 0], reduced_vectors[2:, 1], color='blue', label='Text Word Vectors', alpha=0.6)  # noqa: E501
 
         # Plot positive and negative sentiment vectors
-        plt.scatter(reduced_vectors[0, 0], reduced_vectors[0, 1], color='green', label='Positive Sentiment')
-        plt.scatter(reduced_vectors[1, 0], reduced_vectors[1, 1], color='red', label='Negative Sentiment')
+        plt.scatter(reduced_vectors[0, 0], reduced_vectors[0, 1], color='green', label='Positive Sentiment')  # noqa: E501
+        plt.scatter(reduced_vectors[1, 0], reduced_vectors[1, 1], color='red', label='Negative Sentiment')  # noqa: E501
 
         plt.title('2D PCA of Positive, Negative, and Text Word Vectors')
         plt.xlabel('Principal Component 1')
@@ -159,30 +157,29 @@ class SentimentAnalyser:
         plt.legend()
         plt.show()
 
-    def calculate_article_sentiment(self, articles_word_vectors, negative_sentiment_word_vector, positive_sentiment_word_vector, neutral_threshold=0.05):
-        """Calculate the sentiment (positive, negative, or neutral) of articles based on the distance to sentiment vectors."""
-        
+    def calculate_article_sentiment(self, articles_word_vectors, negative_sentiment_word_vector, positive_sentiment_word_vector, neutral_threshold=0.05):  # noqa: E501
+        """Calculate the sentiment (positive, negative, or neutral) of articles based on the distance to sentiment vectors."""  # noqa: E501
+
         vector_dim = positive_sentiment_word_vector.shape[0]
         if negative_sentiment_word_vector.shape[0] != vector_dim:
-            raise ValueError("Dimensionality mismatch: positive and negative sentiment vectors must have the same dimensions.")
-        
+            raise ValueError("Dimensionality mismatch: positive and negative sentiment vectors must have the same dimensions.")  # noqa: E501
+
         article_sentiments_string = []
         article_sentiments = []
 
-        
         for article in articles_word_vectors:
             pos_distances = []
             neg_distances = []
-            
+
             for sentence_vector in article:
                 sentence_vector = np.array(sentence_vector)
 
                 if len(sentence_vector) != vector_dim:
-                    raise ValueError("Dimensionality mismatch: sentence vectors must have the same dimensions as the sentiment vectors.")
-                
-                # Calculate Euclidean distance between the sentence vector and sentiment vectors
-                pos_distance = np.linalg.norm(sentence_vector - positive_sentiment_word_vector)
-                neg_distance = np.linalg.norm(sentence_vector - negative_sentiment_word_vector)
+                    raise ValueError("Dimensionality mismatch: sentence vectors must have the same dimensions as the sentiment vectors.")  # noqa: E501
+
+                # Calculate Euclidean distance between the sentence vector and sentiment vectors  # noqa: E501
+                pos_distance = np.linalg.norm(sentence_vector - positive_sentiment_word_vector)  # noqa: E501
+                neg_distance = np.linalg.norm(sentence_vector - negative_sentiment_word_vector)  # noqa: E501
 
                 pos_distances.append(pos_distance)
                 neg_distances.append(neg_distance)
@@ -207,30 +204,29 @@ class SentimentAnalyser:
         # print(article_sentiments_string)
         return article_sentiments
 
-
-    def evaluate_sentiment_predictions(self, sentiment_labels, article_sentiments):
+    def evaluate_sentiment_predictions(self, sentiment_labels, article_sentiments):  # noqa: E501
         """
         Evaluate the performance of the sentiment prediction using evaluation metrics.
-        
+
         Parameters:
         - sentiment_labels: List of true sentiment labels (from the labeled dataset).
-        - article_sentiments: List of predicted sentiment labels (from the sentiment model).
-        
+        - article_sentiments: List of predicted sentiment labels (from the sentiment model).  # noqa: E501
+
         Returns:
-        - A dictionary containing accuracy, precision, recall, F1 score for each sentiment class, and the confusion matrix.
+        - A dictionary containing accuracy, precision, recall, F1 score for each sentiment class, and the confusion matrix.  # noqa: E501
         """
-        
+
         if len(sentiment_labels) != len(article_sentiments):
             print(len(sentiment_labels))
             print(len(article_sentiments))
-            raise ValueError("The length of sentiment_labels and article_sentiments must be the same.")
-        
+            raise ValueError("The length of sentiment_labels and article_sentiments must be the same.")  # noqa: E501
+
         accuracy = accuracy_score(sentiment_labels, article_sentiments)
-        precision = precision_score(sentiment_labels, article_sentiments, average='weighted')
-        recall = recall_score(sentiment_labels, article_sentiments, average='weighted')
+        precision = precision_score(sentiment_labels, article_sentiments, average='weighted')  # noqa: E501
+        recall = recall_score(sentiment_labels, article_sentiments, average='weighted')  # noqa: E501
         f1 = f1_score(sentiment_labels, article_sentiments, average='weighted')
 
-        report = classification_report(sentiment_labels, article_sentiments, target_names=["positive", "negative", "neutral"])
+        report = classification_report(sentiment_labels, article_sentiments, target_names=["positive", "negative", "neutral"])  # noqa: E501
         conf_matrix = confusion_matrix(sentiment_labels, article_sentiments)
 
         print("Classification Report:")
@@ -239,12 +235,11 @@ class SentimentAnalyser:
         print(conf_matrix)
 
         # Plot confusion matrix
-        disp = ConfusionMatrixDisplay(confusion_matrix=conf_matrix, 
-                                    display_labels=["positive", "negative", "neutral"])
+        disp = ConfusionMatrixDisplay(confusion_matrix=conf_matrix, display_labels=["positive", "negative", "neutral"])  # noqa: E501
         disp.plot(cmap=plt.cm.Blues)
         plt.title('Confusion Matrix')
         plt.show()
-        
+
         return {
             'accuracy': accuracy,
             'precision': precision,
